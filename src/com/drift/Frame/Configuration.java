20921a29e5739908d593eee90d89fac908b62afe
elloray <elliott.ray@qq.com>
2015-04-07 20:02:28 +0800

    mater

--- src/com/drift/Frame/Configuration.java
+++ src/com/drift/Frame/Configuration.java
@@ -6,5 +6,27 @@ package com.drift.Frame;
  */
 public class Configuration {
 	
+	//线程数量
+	private int ThreadNum;
+	
+	//dubbo配置文件
+	private String dubbo_path;
+
+	public int getThreadNum() {
+		return ThreadNum;
+	}
+
+	public void setThreadNum(int threadNum) {
+		ThreadNum = threadNum;
+	}
+
+	public String getDubbo_path() {
+		return dubbo_path;
+	}
+
+	public void setDubbo_path(String dubbo_path) {
+		this.dubbo_path = dubbo_path;
+	}
+	
 	
 }
